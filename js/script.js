4877b6a0fcccb281b27815265db15717d70ea58f
abeltruong <abel.t@beginningboutique.com.au>
2024-06-01 16:20:19 +0700

    update layout 01/6/24

--- js/script.js
+++ js/script.js
@@ -50,7 +50,7 @@ overlay.addEventListener("click", testimonialsModalFunc)
 // custom select variables
 const select = document.querySelector("[data-select]")
 const selectItems = document.querySelectorAll("[data-select-item]")
-const selectValue = document.querySelector("[data-selecct-value]")
+const selectValue = document.querySelector("[data-select-value]")
 const filterBtn = document.querySelectorAll("[data-filter-btn]")
 
 select.addEventListener("click", function () {
