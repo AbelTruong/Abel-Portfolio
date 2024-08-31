85ba385057d6a57f26157bd79369b59ccb3b4d74
abeltruong <abel.t@beginningboutique.com.au>
2024-08-31 19:52:44 +0700

    format source

--- js/showModalAvatar.js
+++ js/showModalAvatar.js
@@ -1,21 +1,21 @@
-const modalContainer = document.querySelector("[data-modal-avatar-container]");
-const modalCloseBtn = document.querySelector("[data-modal-avatar-close-btn]");
-const overlay = document.querySelector("[data-avatar-overlay]");
-const modalImg = document.querySelector("[modal-avatar-img]");
+const modalContainer = document.querySelector('[data-modal-avatar-container]');
+const modalCloseBtn = document.querySelector('[data-modal-avatar-close-btn]');
+const overlay = document.querySelector('[data-avatar-overlay]');
+const modalImg = document.querySelector('[modal-avatar-img]');
 
 const toggleModal = function () {
-  modalContainer.classList.toggle("active");
-  overlay.classList.toggle("active");
+  modalContainer.classList.toggle('active');
+  overlay.classList.toggle('active');
 };
 
 function closeModal() {
-  modalCloseBtn.addEventListener("click", toggleModal);
-  overlay.addEventListener("click", toggleModal);
+  modalCloseBtn.addEventListener('click', toggleModal);
+  overlay.addEventListener('click', toggleModal);
 }
 
 function showModalAvatar(modalAvatar) {
-  modalImg.src = modalAvatar.querySelector("[data-modal-avatar]").src;
-  modalImg.alt = modalAvatar.querySelector("[data-modal-avatar]").alt;
+  modalImg.src = modalAvatar.querySelector('[data-modal-avatar]').src;
+  modalImg.alt = modalAvatar.querySelector('[data-modal-avatar]').alt;
 
   toggleModal();
   closeModal();
